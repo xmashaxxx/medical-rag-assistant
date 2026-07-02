3ef155a41e39ac8fb07898ba6ccb41ffe0a1c9f5
xmashaxxx <mashamariakrasnova@gmail.com>
2026-07-02 14:07:53 +0300

    Switch to langchain-community embeddings to fix Streamlit Cloud install

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ st.set_page_config(
 try:
     from langchain_community.document_loaders import PyMuPDFLoader
     from langchain_text_splitters import RecursiveCharacterTextSplitter
-    from langchain_huggingface import HuggingFaceEmbeddings
+    from langchain_community.embeddings import HuggingFaceEmbeddings
     from langchain_chroma import Chroma
     RAG_AVAILABLE = True
 except ImportError:
